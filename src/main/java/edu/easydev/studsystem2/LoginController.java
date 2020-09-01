8d683b3cefe67dd0e485953f79ee3367b3d76ab8
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-02 00:10:32 +0300

    edit: changed icon

--- src/main/java/edu/easydev/studsystem2/LoginController.java
+++ src/main/java/edu/easydev/studsystem2/LoginController.java
@@ -1,5 +1,14 @@
 package edu.easydev.studsystem2;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    public TextField loginField;
+    public PasswordField passwordField;
+    public Button okButton;
+    public Button cancelButton;
 
 }
