cc36823592865efcf4191205fbf869679bee6289
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-01 14:20:05 +0300

    add: icon edit: added icon to App

--- src/main/java/edu/easydev/studsystem2/App.java
+++ src/main/java/edu/easydev/studsystem2/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
+        Image icon = new Image(App.class.getResource("icon.png").toExternalForm());
+        stage.getIcons().add(icon);
         stage.show();
     }
 
