e6d6eaa996a54a7adaf26ba660e391bcc3209348
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-03 09:55:32 +0300

    edit: added driver connection test

--- src/test/java/edu/easydev/studsystem2/MySQLTest.java
+++ src/test/java/edu/easydev/studsystem2/MySQLTest.java
@@ -1,7 +1,17 @@
 package edu.easydev.studsystem2;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySQLTest {
-
+    @Test
+    public void testConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            fail("MySQL driver is not connected");
+        }
+    }
 }
